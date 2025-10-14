ee23edca5912edd516b66d85e65890bb48ed5cfd
esdrft <kotika.nyan@gmail.com>
2025-10-14 23:06:18 +0800

    Add Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerController
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed;
+        private Vector2 direction;
+        private Rigidbody2D rb;
+
+
+        private Animator animator;
+
+        private Vector3 TPosition;
+        private bool IsWalking = false;
+
+       
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+        }
+
+
+        void Update()
+        {
+            direction.x = Input.GetAxisRaw("Horizontal");
+            direction.y = Input.GetAxisRaw("Vertical");
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+
+            if (Input.GetMouseButton(0))
+            {
+                TriggerPosition();
+            }
+
+            if (IsWalking)
+            {
+                ItsMove();
+                animator.SetBool("IsWalking", true);
+            }
+
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+
+            if (direction.x == 0 & direction.y == 0)
+            {
+                animator.SetBool("IsWalking", false);
+            }
+            else
+            {
+                animator.SetBool("IsWalking", true);
+            }
+        }
+
+        void TriggerPosition()
+        {
+            TPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TPosition.z = transform.position.z;
+
+            IsWalking = true;
+        }
+
+        void ItsMove()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, TPosition, speed * Time.deltaTime);
+
+            if (transform.position == TPosition)
+            {
+                IsWalking = false;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/player_control.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour
+{
+    public float speed;
+    private float moveInput;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+    private bool facingUp = true;
+ 
+
+    private void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(moveInput * speed, moveInput * speed);
+        if (facingUp == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingUp == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingUp = !facingUp;
+        Vector3 Scaler = transform.localScale;
+        Scaler.y *= -1;
+        transform.localScale = Scaler;
+
+    }
+}
